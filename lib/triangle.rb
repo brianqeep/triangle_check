a87ad87b4893eb023283495f6c8983fc2a64c719
brianqeep <brites009@gmail.com>
2017-02-13 11:06:35 +0300

    pass second test

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,14 +1,14 @@
 class Triangle
   define_method(:initialize) do |length, width, height|
-    @length
-    @width
-    @height
+    @length = length
+    @width = width
+    @height = height
   end
   define_method(:triangle?) do
-    if @length=0 || @width=0 || @height=0
-      false
-    else
+    if @length!=0 && @width!=0 && @height!=0
       true
+    else
+      false
     end
   end
 end
--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -4,12 +4,12 @@ require('triangle')
 describe(Triangle) do
   describe('#triangle?') do
     it("returns false if it is not a triangle") do
-      test_triangle = Triangle.new(0,6,5)
-      expect(test_triangle.triangle?()).to(eq(false))
+      test_triangle = Triangle.new(6,6,5)
+      expect(test_triangle.triangle?()).to(eq(true))
     end
     it("returns true if it is a triangle") do
-      test_triangle = Triangle.new(4,6,5)
-      expect(test_triangle.triangle?()).to(eq(true))
+      test_triangle = Triangle.new(0,6,5)
+      expect(test_triangle.triangle?()).to(eq(false))
     end
   end
 end
